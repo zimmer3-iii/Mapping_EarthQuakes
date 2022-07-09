cde1b89dd33d0c12deaf025af22e046f789ea13c
Steve Zimmermann <zimmer3.iii@gmail.com>
2022-07-09 14:10:24 -0700

    Removed Config.js

--- Simple_Map/static/js/config.js
+++
@@ -1,4 +0,0 @@
-// Add console.log to check to see if our code is working.
-console.log("working key");
-//API Key
-const API_KEY = "pk.eyJ1IjoiemltbWVyMy1paWkiLCJhIjoiY2w1ZWJpN3diMDljZzNrcDluYTRhMHVneCJ9.B5a85tIoNalcxt2cRgbIWA";
