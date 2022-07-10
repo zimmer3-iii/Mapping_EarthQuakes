c8154a616557ac94bed284194f18133b14fa9bb1
Steve Zimmermann <zimmer3.iii@gmail.com>
2022-07-10 16:43:20 -0700

    Add ledgend, Not fully working

--- Earthquakes_past7days_Markers/static/js/logicMarkerLayer.js
+++ Earthquakes_past7days_Markers/static/js/logicMarkerLayer.js
@@ -38,6 +38,15 @@ let map = L.map('mapid', {
     layers: [satelliteStreets]
 })
 
+// Create a legend control object.
+let legend = L.control({
+  position: "bottomright"
+});
+
+// Then add all the details for the legend.
+legend.onAdd = function() {
+  let div = L.DomUtil.create("div", "info legend");
+};
 
 // Then we add a control to the map that will allow the user to change
 // which layers are visible.
@@ -109,39 +118,4 @@ L.geoJSON(data, {
 
       //Then we add the earthquake layer to our map
       earthquakes.addTo(map);
-
-
-  // Create a legend control object.
-  let legend = L.control({
-    position: "bottomright"
   });
-  // Then add all the details for the legend.
-  legend.onAdd = function() {
-    let div = L.DomUtil.create("div", "info legend");
-    const magnitudes = [0, 1, 2, 3, 4, 5];
-    const colors = [
-      "#98ee00",
-      "#d4ee00",
-      "#eecc00",
-      "#ee9c00",
-      "#ea822c",
-      "#ea2c2c"
-    ];
-    // Looping through our intervals to generate a label with a colored square for each interval.
-    for (var i = 0; i < magnitudes.length; i++) {
-      console.log(colors[i]);
-      div.innerHTML +=
-       "<i style='background: " + colors[i] + "'></i> " +
-       magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + "<br>" : "+");
-   }
-    return div;
-  };
-  
-  legend.addTo(map);
-
-
-
-
-  });
-
-
