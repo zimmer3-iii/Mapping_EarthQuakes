ac8d97f7df4363a400fdb15ba5b622cbc66ad92c
Steve Zimmermann <zimmer3.iii@gmail.com>
2022-07-10 15:52:22 -0700

    Added color condition to markers

--- Earthquakes_past7days_Markers/static/js/logic.js
+++ Earthquakes_past7days_Markers/static/js/logic.js
@@ -36,18 +36,39 @@ L.control.layers(baseMaps).addTo(map);
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data)
  {
 // This function returns the style data for each of the earthquakes we plot on
-// the map. We pass the magnitude of the earthquake into a function
-// to calculate the radius.
+// the map. We pass the magnitude of the earthquake into two separate functions
+// to calculate the color and radius.
 function styleInfo(feature) {
   return {
     opacity: 1,
     fillOpacity: 1,
-    fillColor: "#ffae42",
+    fillColor: getColor(feature.properties.mag),
     color: "#000000",
     radius: getRadius(feature.properties.mag),
     stroke: true,
     weight: 0.5
   };
+}
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
+
 // This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
 function getRadius(magnitude) {
@@ -56,7 +77,7 @@ function getRadius(magnitude) {
   }
   return magnitude * 4;
 }
-}
+
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJSON(data, {
 
